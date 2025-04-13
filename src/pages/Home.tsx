c07bf1e0d2c4f1d0746ea88952139c598edaa2f3
NickFeige <nfeige7@gmail.com>
2025-04-13 03:48:38 +0100

    added about page routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="main-background">
+  return (
+    <div className="main-background">
+      <h1 className="title">Grip Climbing</h1>
 
-            <h1 className="title"> <i>Grip Climbing</i> </h1>
-
-            <nav className="navbar">
-                <button className="button"><i>Categories</i></button>
-                <button className="button"><i>About</i></button>
-                <button className="button" onClick={() => navigate("/cart")}>
-                    <i>Cart</i>
-                </button>
-            </nav>
-        </div>
-    );
+      <nav className="navbar">
+        <button className="button">Categories</button>
+        <button className="button" onClick={() => navigate("/about")}>
+          <i>About</i>
+        </button>
+        <button className="button" onClick={() => navigate("/cart")}>
+          <i>Cart</i>
+        </button>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
+import About from "../pages/About"; // 👈 Add this line
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/about" element={<About />} /> {/* 👈 Add this route */}
     </Routes>
   );
 };
