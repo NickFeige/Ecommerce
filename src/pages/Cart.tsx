f454d631c78de26a95dadfcfac38a4b79eb534ee
NickFeige <nfeige7@gmail.com>
2025-04-13 03:08:27 +0100

    added  background color and added routing

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const Cart: React.FC = () => {
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      {/* Add cart items here */}
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="main-background">
+
+            <h1 className="title"> <i>Grip Climbing</i> </h1>
+
+            <nav className="navbar">
+                <button className="button"><i>Categories</i></button>
+                <button className="button"><i>About</i></button>
+                <button className="button" onClick={() => navigate("/cart")}>
+                    <i>Cart</i>
+                </button>
+            </nav>
+        </div>
+    );
+};
+
+export default Home;
